549ea842ff29b49f1eef0a07434d493988a66f1e
Penelope Drago <prd@drago.com.au>
2023-06-18 20:38:06 +1000

    Add Home component and CSS base styles

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
+import React from 'react';
+// install react router dom and import Route and Switch
 import Header from './Header';
+import Home from './Home';
 import Footer from './Footer';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <main class="main-content">
-        <p>Text paragraph</p>
-        <p>Text paragraph</p>
+      <main className="main-content">
+        {/* Include switch component */}
+        <Home />
       </main>
       <Footer />
     </div>

b7cbb781215be660dfed6767c92c743c9d820c28
Penelope Drago <prd@drago.com.au>
2023-06-18 22:00:47 +1000

    Add Food and Bookings components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React from 'react';
 // install react router dom and import Route and Switch
 import Header from './Header';
 import Home from './Home';
+import Food from './Food';
+import Bookings from './Bookings';
 import Footer from './Footer';
 
 function App() {
@@ -12,6 +14,8 @@ function App() {
       <main className="main-content">
         {/* Include switch component */}
         <Home />
+        <Food />
+        <Bookings />
       </main>
       <Footer />
     </div>

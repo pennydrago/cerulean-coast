a365a5759aaf07e8948bf1c99920cc79089fa84a
Penelope Drago <prd@drago.com.au>
2023-06-28 14:07:28 +1000

    add text data to dessertItems file

--- src/components/data/dessertItems.js
+++ src/components/data/dessertItems.js
@@ -1,9 +1,30 @@
 export const dessertItems = [
   {
     id: 1,
-    name: "name",
-    description: "description",
+    name: "Chocolate tart",
+    description: "A rich chocolate tart; can be served with cream or ice cream.",
     imageSource: "url",
-    imageAltText: "alt text"
+    imageAltText: "A piece of chocolate tart served from the whole chocolate tart"
+  },
+  {
+    id: 2,
+    name: "Pancakes",
+    description: "A stack of fluffy pancakes, with fruit and syrup.",
+    imageSource: "url",
+    imageAltText: "A stack of pancakes with berries on the side"
+  },
+  {
+    id: 3,
+    name: "Custard cake",
+    description: "A custard cake topped with meringue and served with cream on the side.",
+    imageSource: "url",
+    imageAltText: "A custard cake with meringue and lemon on top."
+  },
+  {
+    id: 4,
+    name: "Raspberry tart",
+    description: "A raspberry tart created with a biscuit base and vanilla custard balanced with juicy raspberries.",
+    imageSource: "url",
+    imageAltText: "A vanilla custard tart topped with raspberries."
   }
 ]

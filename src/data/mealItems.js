69777a12611a7a8a33b6a5dc89ea26d18575da86
Penelope Drago <prd@drago.com.au>
2023-06-28 14:33:19 +1000

    add text data to mealItems file

--- src/data/mealItems.js
+++ src/data/mealItems.js
@@ -1,9 +1,37 @@
 export const mealItems = [
   {
     id: 1,
-    name: "name",
-    description: "description",
+    name: "Vegetable salad",
+    description: "A mix of green salad leaves, beetroot, orange, fetta cheese and walnuts.",
     imageSource: "url",
-    imageAltText: "alt text"
+    imageAltText: "A bowl containing a vegetable salad with oranges and walnuts"
+  },
+  {
+    id: 2,
+    name: "Vegetable stirfry",
+    description: "Stir-fried vegatables, including broccoli, red capscicum, mushrooms, onions and spring onions; served with noodles",
+    imageSource: "url",
+    imageAltText: "A plate containing a vegetable stirfry on noodles"
+  },
+  {
+    id: 3,
+    name: "Fish with beans and rice",
+    description: "Pan-fried cod with a tomato and capers sauce; with green beans and white rice.",
+    imageSource: "url",
+    imageAltText: "A plate of cod, green beans and white rice"
+  },
+  {
+    id: 4,
+    name: "Prawn and vegetable stirfry",
+    description: "Prawns, capscicums and onions combined in a spicy sauce.",
+    imageSource: "url",
+    imageAltText: "A plate containing a spicy prawn and vegetable stirfry"
+  },
+  {
+    id: 5,
+    name: "Tomato and basil pizza",
+    description: "A fluffy pizza base topped with tomato sauce, mozzarella cheese and basil leaves.",
+    imageSource: "url",
+    imageAltText: "A whole pizza topped with tomato sauce, melted mozzarella cheese and basil leaves"
   }
 ]

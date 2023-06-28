c9607af074f65420ae9d0d65375fc7705a7ff041
Penelope Drago <prd@drago.com.au>
2023-06-28 15:57:34 +1000

    add image URLs to data files

--- src/data/dessertItems.js
+++ src/data/dessertItems.js
@@ -3,28 +3,28 @@ export const dessertItems = [
     id: 1,
     name: "Chocolate tart",
     description: "A rich chocolate tart; can be served with cream or ice cream.",
-    imageSource: "url",
+    imageSource: "../images/dessertItemImages/razvan-mirel-ufK6dVE4KHA-unsplash.jpg",
     imageAltText: "A piece of chocolate tart served from the whole chocolate tart"
   },
   {
     id: 2,
     name: "Pancakes",
     description: "A stack of fluffy pancakes, with fruit and syrup.",
-    imageSource: "url",
+    imageSource: "../images/dessertItemImages/luke-pennystan-09FcOqmi8R0-unsplash.jpg",
     imageAltText: "A stack of pancakes with berries on the side"
   },
   {
     id: 3,
     name: "Custard cake",
     description: "A custard cake topped with meringue and served with cream on the side.",
-    imageSource: "url",
+    imageSource: "../images/dessertItemImages/13on-z4BJGODf6O4-unsplash.jpg",
     imageAltText: "A custard cake with meringue and lemon on top."
   },
   {
     id: 4,
     name: "Raspberry tart",
     description: "A raspberry tart created with a biscuit base and vanilla custard balanced with juicy raspberries.",
-    imageSource: "url",
+    imageSource: "../images/dessertItemImages/alexandra-kusper-88sNfcLB_KM-unsplash.jpg",
     imageAltText: "A vanilla custard tart topped with raspberries."
   }
 ]
--- src/data/mealItems.js
+++ src/data/mealItems.js
@@ -3,35 +3,35 @@ export const mealItems = [
     id: 1,
     name: "Vegetable salad",
     description: "A mix of green salad leaves, beetroot, orange, fetta cheese and walnuts.",
-    imageSource: "url",
+    imageSource: "../images/mealItemImages/louis-hansel-MlPD-AzZYMg-unsplash.jpg",
     imageAltText: "A bowl containing a vegetable salad with oranges and walnuts"
   },
   {
     id: 2,
     name: "Vegetable stirfry",
     description: "Stir-fried vegatables, including broccoli, red capscicum, mushrooms, onions and spring onions; served with noodles",
-    imageSource: "url",
+    imageSource: "../images/mealItemImages/hermes-rivera-k-sTTFdcZek-unsplash.jpg",
     imageAltText: "A plate containing a vegetable stirfry on noodles"
   },
   {
     id: 3,
     name: "Fish with beans and rice",
     description: "Pan-fried cod with a tomato and capers sauce; with green beans and white rice.",
-    imageSource: "url",
+    imageSource: "../images/mealItemImages/david-b-townsend-AnC569NdAJc-unsplash.jpg",
     imageAltText: "A plate of cod, green beans and white rice"
   },
   {
     id: 4,
     name: "Prawn and vegetable stirfry",
     description: "Prawns, capscicums and onions combined in a spicy sauce.",
-    imageSource: "url",
+    imageSource: "../images/mealItemImages/alyssa-kowalski-kQhCD2q9_XI-unsplash.jpg",
     imageAltText: "A plate containing a spicy prawn and vegetable stirfry"
   },
   {
     id: 5,
     name: "Tomato and basil pizza",
     description: "A fluffy pizza base topped with tomato sauce, mozzarella cheese and basil leaves.",
-    imageSource: "url",
+    imageSource: "../images/mealItemImages/razvan-cristea-V-MaZ44zMLk-unsplash.jpg",
     imageAltText: "A whole pizza topped with tomato sauce, melted mozzarella cheese and basil leaves"
   }
 ]

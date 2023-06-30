cd2ae5bc31799dbf8dc136ab43175e4ba49d82d7
Penelope Drago <prd@drago.com.au>
2023-06-30 14:55:34 +1000

    Add BookingCard component and CSS styles

---
+++ src/components/BookingCard.js
@@ -0,0 +1,9 @@
+export default function BookingCard() {
+  return (
+    <div class="booking-card">
+      <p>Date: 12/01/2024</p>
+      <p>Time: 12:00pm</p>
+      <p>Number of people: 4</p>
+    </div>
+  );
+}
--- src/components/Bookings.js
+++ src/components/Bookings.js
@@ -1,4 +1,5 @@
 import BookingForm from "./BookingForm";
+import BookingCard from "./BookingCard";
 
 export default function Bookings() {
   return (
@@ -6,6 +7,14 @@ export default function Bookings() {
       <div className="container">
         <h2>Bookings</h2>
         <BookingForm />
+        <h3>Confirmed bookings</h3>
+        <div class="container-flex">
+          <BookingCard />
+          <BookingCard />
+          <BookingCard />
+          <BookingCard />
+          <BookingCard />
+        </div>
       </div>
     </div>
   );

1ebb243082f89a8166aadf3cdc617dae9e220590
Penelope Drago <prd@drago.com.au>
2023-07-11 16:41:34 +1000

    Add horizontal rules to Home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,10 +7,12 @@ export default function Home() {
       <div className="container">
         <p>Our restaurant is located in Coastal Town, with spectacular views of the ocean. We welcome you to enjoy a meal from a variety of choices for lunch or dinner. We are also open for afternoon refreshments.</p>
         <p>We aim to create a chilled atmosphere of delicious food and stunning views over the water.</p>
+        <hr />
         {/* Open hours */}
         <h3>Open hours</h3>
         <p>Monday to Sunday</p>
         <p>12pm to 10pm</p>
+        <hr />
         {/* Contact details */}
         <h3>Contact us</h3>
         <p>25 Ocean Boulevard</p>

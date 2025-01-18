2b00cb7de276a03de2c83493cd81273ed9243541
Penelope Drago <prd@drago.au>
2025-01-18 17:16:56 +1100

    update current year in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <div>
       <div className="container" id="footer">
-        <p>Designed and developed by Penny Drago &copy; 2023</p>
+        <p>Designed and developed by Penny Drago &copy; 2023&ndash;2025</p>
       </div>
     </div>
   );
